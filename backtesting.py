db03b010d9871024b80c6de8e30a7d51832e16ef
cryptphnx <113685343+cryptphnx@users.noreply.github.com>
2025-02-18 00:29:59 +0100

    Update backtesting.py

--- backtesting.py
+++ backtesting.py
@@ -4,15 +4,16 @@ from data import get_historical_data
 from strategy import PineStrategy
 
 def run_backtest(symbol='BTCUSDT', timeframe='5m', start_str='1 month ago UTC', strategy_params={}):
-    cerebro = bt.Cerebro(optreturn=False)
+    cerebro = bt.Cerebro()
+    cerebro.optreturn = False  # might help if we want certain data
     cerebro.addstrategy(PineStrategy, **strategy_params)
     
     df = get_historical_data(symbol=symbol, interval=timeframe, start_str=start_str)
     data = bt.feeds.PandasData(dataname=df)
     cerebro.adddata(data)
     
-    cerebro.broker.setcommission(commission=0.00055)  # 0.055%
-    
+    cerebro.broker.setcommission(commission=0.00055)  # 0.055% for example
+
     initial_value = cerebro.broker.getvalue()
     results = cerebro.run()
     final_value = cerebro.broker.getvalue()
@@ -24,9 +25,9 @@ def run_backtest(symbol='BTCUSDT', timeframe='5m', start_str='1 month ago UTC', 
 
 if __name__ == "__main__":
     init_val, final_val, trades, cerebro = run_backtest()
-    print(f"Initial Portfolio Value: {init_val}")
-    print(f"Final Portfolio Value: {final_val}")
-    print("Trade Log:")
-    for trade in trades:
-        print(trade)
+    print(f"Initial Value: {init_val}")
+    print(f"Final Value: {final_val}")
+    print("Trades:")
+    for t in trades:
+        print(t)
     cerebro.plot(style='candlestick')
